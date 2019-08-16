70dd11880affe975f8215124d1eddd92d9f0a811
Queck <yeungj1602@gmail.com>
2019-08-16 14:40:47 +1200

    Added car

---
+++ Dangerous Driving/Assets/Car folder/Car.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Car speed and steering
+    [SerializeField] float accelerate = 100f;
+    [SerializeField] float steer = 50f;
+
+    // Car sounds
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+
+    // Car particles
+    [SerializeField] ParticleSystem wheelParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem completeLapParticles;
+
+    // Car attributes
+    Rigidbody carBody;
+    AudioSource audioSource;
+    bool isTranscending = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void RespondToAccelerateInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            ApplyAcceleration();
+        }
+        else
+        {
+            audioSource.Stop();
+            //TODO:  Particles stop
+        }
+    }
+
+    private void RespondToSteerInput()
+    {
+        
+
+        // Left Steer
+        if (Input.GetKey(KeyCode.A))
+        {
+            //carBody.freezeRotation = true; // manual control
+
+            float steerThisFrame = steer * Time.deltaTime;
+
+            transform.Rotate(Vector3.down * steerThisFrame);
+            //carBody.freezeRotation = false; // physics control
+        }
+        // Right Steer
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //carBody.freezeRotation = true; // manual control
+            float steerThisFrame = steer * Time.deltaTime;
+
+            transform.Rotate(Vector3.up * steerThisFrame);
+            //carBody.freezeRotation = false; // physics control
+        }
+        
+
+        
+    }
+
+    private void RespondToCameraInput()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+
+        }
+
+    }
+
+    private void ApplyAcceleration()
+    {
+        carBody.AddRelativeForce(Vector3.forward * accelerate * Time.deltaTime);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RespondToAccelerateInput();
+        RespondToSteerInput();
+    }
+}
---
+++ Dangerous Driving/Assets/Car folder/CarController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float m_horizontalInput;
+    private float m_verticalInput;
+    private float m_steeringAngle;
+
+    public WheelCollider frontDriverW, frontPassengerW;
+    public WheelCollider rearDriverW, rearPassengerW;
+    public Transform frontDriverT, frontPassengerT;
+    public Transform rearDriverT, rearPassengerT;
+
+    public float maxSteerAngle = 30f;
+    public float motorForce = 300f;
+
+    public void GetInput()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticalInput = Input.GetAxis("Vertical");
+
+    }
+
+    public void Steer()
+    {
+        m_steeringAngle = maxSteerAngle * m_horizontalInput;
+        frontDriverW.steerAngle = m_steeringAngle;
+        frontPassengerW.steerAngle = m_steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontDriverW.motorTorque = m_verticalInput * motorForce;
+        frontPassengerW.motorTorque = m_verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontDriverW, frontDriverT);
+        UpdateWheelPose(frontPassengerW, frontPassengerT);
+        UpdateWheelPose(rearDriverW, rearDriverT);
+        UpdateWheelPose(rearPassengerW, rearPassengerT);
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos = transform.position;
+        Quaternion quat = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        //UpdateWheelPoses();
+    }
+}
