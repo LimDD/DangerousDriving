5b9cbf866f1c7bfe78b3b68fbf3850394f2b224b
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2019-09-03 07:25:37 +1200

    added asteroid and adjusted camera

---
+++ Dangerous Driving/Assets/Asset/Scripts/AsteroidHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidHit : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject prefab;
+    public Vector3 position;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (this.gameObject == false)
+        {
+            position = new Vector3(Random.Range(1, 20), 50, Random.Range(1, 20));
+            
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+        this.gameObject.SetActive(false);
+    }
+}
