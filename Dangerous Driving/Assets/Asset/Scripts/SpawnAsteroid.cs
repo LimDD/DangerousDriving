2d543966c5e380a2bc3e93ef9d38226faeef78cf
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2019-09-05 00:40:15 +1200

    Added explosions to asteroids
    Explosions also destroy only the clones after 2 seconds not the gameobject itself.

--- Dangerous Driving/Assets/Asset/Scripts/SpawnAsteroid.cs
+++ Dangerous Driving/Assets/Asset/Scripts/SpawnAsteroid.cs
@@ -6,20 +6,20 @@ public class SpawnAsteroid : MonoBehaviour
 {
     public GameObject asteroidPrefab;
     public GameObject carCordinate;
-    public float spawnTimer = 0.7f;
+    public float spawnTimer = 4f;
     public Vector3 positionOfCar;
     void Start()
     {
         StartCoroutine(asteroidLoop());
         
     }
-    void Update()
+    void LateUpdate()
     {
         this.positionOfCar = carCordinate.transform.position;
     }
     private void spawner()
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 3; i++)
         {
             GameObject aster = Instantiate(asteroidPrefab) as GameObject;
             aster.transform.position = new Vector3(this.positionOfCar.x + Random.Range(-50, 50), 60, this.positionOfCar.z + Random.Range(-50, 50));
