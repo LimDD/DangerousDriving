3794b5ea25fe12ed19df2b72ac21ade2c41972d2
james126 <mrjameseason@gmail.com>
2019-09-10 15:56:28 +1200

    Added more health pickups

---
+++ Dangerous Driving/Assets/Editor/NumberMachine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int multiplyvalue(int value)
+    {
+        return value * 5;
+    }
+}
