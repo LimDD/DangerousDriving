a3d13d8c3e58840150632a19b674325c2a860484
james126 <mrjameseason@gmail.com>
2019-09-14 19:56:15 +1200

    More testing

--- Dangerous Driving/Assets/Resources/LapTimer/LapTimer.cs
+++ Dangerous Driving/Assets/Resources/LapTimer/LapTimer.cs
@@ -10,15 +10,53 @@ public class LapTimer : MonoBehaviour
     public float startTimer = 0;
     public float time;
     public string timestring;
+
+    bool startflag;
+    bool trigger1flag;
+    bool finishflag;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startflag = false;
+        trigger1flag = false;
+        finishflag = false;
+        timestring = null;
+    }
+
+    public void setstartflag()
+    {
+        startflag = true;
+    }
+
+    public void settrigger1flag()
+    {
+        trigger1flag = true;
+    }
+
+    public void setfinishflag()
+    {
+        finishflag = true;
     }
 
     public void SetStartTimerOn()
     {
         startTimer = 1.0f;
+    }
+
+    public bool getstartflag
+    {
+        get { return startflag; }
+    }
+
+    public bool gettrigger1flag
+    {
+        get { return trigger1flag; }
+    }
+
+    public bool getfinishflag
+    {
+        get { return finishflag; }
     }
 
     public string getcurrenttime
---
+++ Dangerous Driving/Assets/Tests/Tests2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Tests2
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Tests2SimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator Tests2WithEnumeratorPasses()
+        {
+            GameObject car = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Car folder/Prefabs/Car"));
+            GameObject laptimer = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Assets/Resources/Asset/Prefabs/LapTimer.prefab"));
+
+            //LapTimer ltscript = car.GetComponent<LapTimer>();
+
+           // bool gamestarted = ltscript.getstartflag;
+
+            yield return null;
+           // if (gamestarted == false)
+            //{
+                //string timevalue = ltscript.getcurrenttime;
+                //Assert.IsNull(timevalue);
+            //}
+
+            bool x = false;
+
+           Assert.IsFalse(x);
+
+        }
+    }
+}
