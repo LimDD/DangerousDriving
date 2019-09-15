5965c91be94296de6f52f78f6756cff32b9d648d
james126 <mrjameseason@gmail.com>
2019-09-15 14:54:04 +1200

    Tests for lap timer

--- Dangerous Driving/Assets/Tests/Tests2.cs
+++ Dangerous Driving/Assets/Tests/Tests2.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -8,39 +9,89 @@ namespace Tests
 {
     public class Tests2
     {
-        public bool gamestarted;
-        // A Test behaves as an ordinary method
-        [Test]
-        public void Tests2SimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator Tests2WithEnumeratorPasses()
+        public IEnumerator LapTimerStarts()
         {
             //instantiate laptimber object
             GameObject laptimer = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Asset/Prefabs/LapTimer"));
 
-            //instantiate car object
-            GameObject car = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Car folder/Prefabs/Car"));
+            //reference LapTimer script
+            LapTimer ltscript = laptimer.GetComponent<LapTimer>();
+
+            //start lap timer
+            ltscript.startTimer = 1;
+
+            //wait 3 seconds
+            yield return new WaitForSeconds(3f);
+
+            //get currentime
+            string timevalue = ltscript.getcurrenttime;
+
+            //check currenttime not null
+            Assert.IsNotNull(timevalue);
+        }
+
+        [UnityTest]
+        public IEnumerator LapTimerStops()
+        {
+            //instantiate laptimber object
+            GameObject laptimer = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Asset/Prefabs/LapTimer"));
 
             //reference LapTimer script
-            LapTimer ltscript = car.GetComponent<LapTimer>();
+            LapTimer ltscript = laptimer.GetComponent<LapTimer>();
 
-            //check if game has started
-            gamestarted = ltscript.getstartflag;
+            //start lap timer
+            ltscript.startTimer = 1;
 
-            yield return null;
+            //wait 3 seconds
+            yield return new WaitForSeconds(3f);
 
-            //game started == false → game not started
-            if (gamestarted == false) 
-            {
-                string timevalue = ltscript.getcurrenttime; //game not started → timevalue of timer = null
-                Assert.IsNull(timevalue); //verify timevalue is null
-            }
+            //stop timer
+            ltscript.startTimer = 0;
+
+            //get currenttime now timer has stopped
+            string timevalue1 = ltscript.getcurrenttime;
+
+            //wait 3 seconds
+            yield return new WaitForSeconds(3f);
+
+            //get current time again
+            string timevalue2 = ltscript.getcurrenttime;
+
+            //check timer has stopped
+            Assert.AreEqual(timevalue1, timevalue2);
+
+            //show that timevalues are not null i.e timer started and then stopped
+            Assert.IsNotNull(timevalue1);
+            Assert.IsNotNull(timevalue2);
+        }
+
+        [UnityTest]
+        public IEnumerator LapTimerAccuracy()
+        {
+            //instantiate laptimber object
+            GameObject laptimer = MonoBehaviour.Instantiate<GameObject>(Resources.Load<GameObject>("Asset/Prefabs/LapTimer"));
+
+            //reference LapTimer script
+            LapTimer ltscript = laptimer.GetComponent<LapTimer>();
+
+            //start lap timer
+            ltscript.startTimer = 1;
+
+            //wait 3 seconds
+            yield return new WaitForSeconds(3f);
+
+            //stop timer
+            ltscript.startTimer = 0;
+
+            //get currenttime now timer has stopped
+            string timevalue = ltscript.getcurrenttime;
+
+            char c = timevalue[6];
+            int x = Convert.ToInt32(new string(c, 1));
+
+            //check that timer seconds value is greater than 2
+            Assert.Greater(c, 2);
         }
     }
 }
