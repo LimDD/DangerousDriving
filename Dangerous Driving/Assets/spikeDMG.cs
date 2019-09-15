6cf85f60484aa8c522b44a65aba9cfdbc235f214
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2019-09-16 09:21:05 +1200

    added spike strips

---
+++ Dangerous Driving/Assets/spikeDMG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeDMG : MonoBehaviour
+{
+    public GameObject car;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
