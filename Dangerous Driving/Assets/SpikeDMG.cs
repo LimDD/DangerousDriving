dedfadf68f1c48c686e3dffc4d90b2eede191362
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2019-09-26 06:18:39 +1200

    added spike damage

---
+++ Dangerous Driving/Assets/SpikeDMG.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDMG : MonoBehaviour
+{
+    public HealthBar health;
+    void Start()
+    {
+        
+    }
+   
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Spike"))
+        {
+            health.UpdateHealth(-10.0f);
+        }
+    }
+}
