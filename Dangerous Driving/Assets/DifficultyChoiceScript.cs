6b0d98082c7ad81203c097d28518b5b8d45dc907
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2019-09-27 19:20:32 +1200

    difficulty implemented

--- Dangerous Driving/Assets/DifficultyChoiceScript.cs
+++ Dangerous Driving/Assets/DifficultyChoiceScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DifficultyChoiceScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    bool isSelected = false;
+    
     void Start()
     {
         
