35b3fb518d7b549f13c780032d1fcdff67b79427
Queck <yeungj1602@gmail.com>
2019-09-30 22:45:40 +1300

    Spikes now slow down car. Fixed car getting stuck on hills

--- Dangerous Driving/Assets/Resources/Asset/Scripts/SpikeDMG.cs
+++ Dangerous Driving/Assets/Resources/Asset/Scripts/SpikeDMG.cs
@@ -1,25 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
 
 public class SpikeDMG : MonoBehaviour
 {
     public HealthBar health;
+    CarController car;
     void Start()
     {
-
+        car = this.GetComponent<CarController>(); // Get the componenet from this gameobject
     }
 
     void Update()
     {
 
     }
+
+    private void RemoveSlowDebuff()
+    {
+        car.m_Topspeed /= 0.5f; // Reverse the slow by dividing
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Spike")) //tag on the object you collide with
         {
             health.UpdateHealth(-10.0f);
+            car.m_Topspeed = car.m_Topspeed * 0.5f; // Slow down the car by decreasing its max speed
+            Invoke("RemoveSlowDebuff", 3f);
         }
+        //else if (other.c)
     }
 
     void OnCollisionEnter(Collision other)
